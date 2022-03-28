2cc5030eb9c74f0ee1bcf92632cd2077f3daf617
dresha <andrii.ponamarov@ankorstore.com>
2022-03-28 15:04:34 +0200

    feat: Add autocomplete

--- pages/users.js
+++ pages/users.js
@@ -1,11 +1,14 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import AppContext from "../context/AppContext";
 import Link from "next/link";
 import RouteGuard from "../guards/RouteGuard";
 import Profile from "../components/Profile";
+import Autosuggest from "react-autosuggest";
 
 const UsersPage = ({ levels, milestones }) => {
   const { data, loading } = useContext(AppContext);
+  const [users, setUsers] = useState([]);
+  const [inputValue, setInputValue] = useState("");
 
   return (
     <RouteGuard>
@@ -14,13 +17,33 @@ const UsersPage = ({ levels, milestones }) => {
       ) : (
         <div>
           <Profile />
-          <ul>
-            {Object.keys(data.users).map((userName) => (
-              <li key={userName}>
-                <Link href={`/user/${userName}`}>{userName}</Link>
-              </li>
-            ))}
-          </ul>
+          <Autosuggest
+            renderSuggestion={(userName) => (
+              <Link href={`/user/${userName}`}>{userName}</Link>
+            )}
+            alwaysRenderSuggestions={true}
+            inputProps={{
+              placeholder: "Type a user name",
+              type: "search",
+              value: inputValue,
+              onChange: (event, { newValue }) => setInputValue(newValue),
+            }}
+            suggestions={users}
+            onSuggestionsFetchRequested={({ value }) => {
+              const usersToDisplay = Object.keys(data.users);
+
+              setUsers(
+                usersToDisplay.filter((user) => {
+                  return user.toLowerCase().indexOf(value.toLowerCase()) !== -1;
+                })
+              );
+            }}
+            onSuggestionsClearRequested={() => {
+              const usersToDisplay = Object.keys(data.users);
+              setUsers(usersToDisplay);
+            }}
+            getSuggestionValue={(value) => value}
+          />
         </div>
       )}
     </RouteGuard>
